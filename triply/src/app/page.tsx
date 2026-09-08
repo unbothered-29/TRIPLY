6d690c69e4306d187dc163034b3ea092c6962278
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-09-08 15:00:04 +0530

    routing

--- triply/src/app/page.tsx
+++ triply/src/app/page.tsx
@@ -16,6 +16,5 @@ export default function Home() {
       <Footer />
     </>
 
-    
   )
 }
--- triply/src/components/PopularDestinations.tsx
+++ triply/src/components/PopularDestinations.tsx
@@ -1,32 +1,40 @@
-import React from 'react';
 import DestinationCard from './DestinationCard';
 
-const PopularDestinations = () => {
+const destinations = [
+  {
+    name: "Tokyo",
+    country: "Japan",
+    slug: "tokyo",
+  },
+  {
+    name: "Rome",
+    country: "Italy",
+    slug: "rome",
+  },
+  {
+    name: "Goa",
+    country: "India",
+    slug: "goa",
+  },
+];
+
+export default function PopularDestinations () {
+
   return (
 <section>
   <h2>Popular Destinations</h2>
 
-  <div>
-        <DestinationCard
-          name="Tokyo"
-          country="Japan"
-          slug="tokyo"
-        />
-
-        <DestinationCard
-          name="Rome"
-          country="Italy"
-          slug="rome"
-        />
-
-        <DestinationCard
-          name="Goa"
-          country="India"
-          slug="goa"
-        />
-  </div>
+<div>
+  {destinations.map((destination)=>(
+    <DestinationCard 
+      key={destination.slug}
+      // name={destination.name} 
+      // country={destination.country}
+      // slug={destination.slug}
+      {...destination}     //spread operator
+    />
+  ))}
+</div>
 </section>
   )
 }
-
-export default PopularDestinations;
