2839cd5f592a09f69fd19f7b749f11ae4e66378b
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-09-08 22:51:32 +0530

    places update

--- triply/src/app/explore/[slug]/page.tsx
+++ triply/src/app/explore/[slug]/page.tsx
@@ -2,6 +2,31 @@ import { destinations } from "@/data/destinations";
 import DestinationHero from "@/components/DestinationHero";
 import PopularPlaces from "@/components/PopularPlaces";
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}:{
+  params:Promise<{slug:string}>;
+}):Promise<Metadata>{
+    const {slug}=await params;
+
+    const destination=destinations.find(
+      (destination) => destination.slug === slug
+      );
+
+  if (!destination) {
+    return {
+      title: "Destination Not Found | Triply",
+    };
+  }
+
+  return {
+    title: `${destination.name} | Triply`,
+    description: destination.description,
+  };
+}
+
 
 export default async function DestinationPage({
   params,
