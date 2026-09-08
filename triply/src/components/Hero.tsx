e12d4b6ab9f5554f4f518753e949fa690be9d43c
Jessicaa-27 <chauhanjessicaa27@gmail.com>
2026-09-08 12:20:41 +0530

    triply

--- triply/src/components/Hero.tsx
+++ triply/src/components/Hero.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Link from 'next/link';
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+
+<section className='mt-10 ml-10'>
+  <p className='font-thin text-4xl'>YOUR ADVENTURE STARTS HERE</p>
+  <h1 className='font-thin text-xl'>Explore the world. <br />Plan your adventure.</h1>
+  <p className='font-thin text-xl'>Discover amazing destinations,save your favorite
+    places,and build your perfect trip.
+  </p>
+
+  <Link href="/explore" 
+  className='font-thin text-3xl'
+  >Explore destinations</Link>
+</section>
+
+  );
 }
 
 export default Hero
--- triply/src/components/Navbar.tsx
+++ triply/src/components/Navbar.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+<nav className='w-full flex justify-between items-end mt-5 ml-5'>
+  <Link href="/"
+  className='font-black text-3xl'
+  >Triply</Link>
+
+  <div className='w-[21%] space-x-5 mr-5'>
+    <Link href="/explore" className='font-thin text-xl'>Explore</Link>
+    <Link href="/trips" className='font-thin text-xl'>My Trips</Link>
+    <Link href="/saved" className='font-thin text-xl'>Saved</Link>
+    <Link href="/login" className='font-thin text-xl'>Login</Link>
+  </div>
+</nav>
   )
 }
 
